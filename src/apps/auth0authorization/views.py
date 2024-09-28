59bb01eaf72f95608f4fbbccdf19f6f35965dd45
pisces2336 <pisces2336@gmail.com>
2024-09-28 22:57:31 +0900

    「APIエンドポイントを保護する」

--- src/apps/auth0authorization/views.py
+++ src/apps/auth0authorization/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def public(request):
+    return JsonResponse(
+        {"message": "Hello from a public endpoint! You don't need to be authenticated to see this."}
+    )
+
+
+@api_view(["GET"])
+def private(request):
+    return JsonResponse(
+        {"message": "Hello from a private endpoint! You need to be authenticated to see this."}
+    )
